2351a47c687519f2609b21ff6b6d9c4cb67fac74
adityaviren <adityaviren@gmail.com>
2020-09-24 16:33:47 +0530

    Uc7 deployed in main with total number of dice rolls counted

--- src/snakeAndLadder/SnakeAndLadder.java
+++ src/snakeAndLadder/SnakeAndLadder.java
@@ -4,21 +4,38 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int position = 0, temp_change_position, temp_position,no_of_dice_rolls=0;
-		while (position != 100) {
-			no_of_dice_rolls++;
-			temp_position = position;
-			temp_change_position = diceRoll() * positionRoll();
-			temp_position += temp_change_position;
-			if (temp_position <= 0) {
-				position = 0;
-			} else if (temp_position > 100) {
-			} else {
-				position = temp_position;
+		int position_1 = 0, position_2 = 0, temp_change_position, temp_position, no_of_dice_rolls_1 = 0,no_of_dice_rolls_2=0;
+		boolean toggle_player = true; // true means player 1
+		while (position_1 != 100 && position_2 != 100) {
+			if (toggle_player) {
+				no_of_dice_rolls_1++;
+				temp_position = position_1;
+				temp_change_position = diceRoll() * positionRoll();
+				temp_position += temp_change_position;
+				if (temp_position <= 0)
+					position_1 = 0;
+				else if (temp_position > 100) {
+				} else
+					position_1 = temp_position;
+				toggle_player = (temp_change_position >0) ? true : false ;
+				System.out.println("Position of player 1 is "+position_1);
 			}
-			System.out.println(position);
+			else {
+				no_of_dice_rolls_2++;
+				temp_position = position_2;
+				temp_change_position = diceRoll() * positionRoll();
+				temp_position += temp_change_position;
+				if (temp_position <= 0)
+					position_2 = 0;
+				else if (temp_position > 100) {
+				} else
+					position_2 = temp_position;
+				toggle_player = (temp_change_position >0) ? false : true ;
+				System.out.println("Position of player 2 is "+position_2);
+			}
 		}
-		System.out.println("You Won in "+no_of_dice_rolls+" dice rolls.");
+		String message = (position_1==100) ? "Player 1 " : "Player 2 ";
+		System.out.println(message+" won the game in "+(no_of_dice_rolls_1+no_of_dice_rolls_2)+" dice rolls");
 	}
 
 	private static int diceRoll() {
@@ -40,8 +57,11 @@ public class SnakeAndLadder {
 		case 5:
 			dice_roll = 5;
 			break;
-		default:
+		case 0:
 			dice_roll = 6;
+			break;
+		default :
+			dice_roll=0;
 			break;
 		}
 		return dice_roll;
