25d46794f991566483d8fde7d3de4279ec5ae61e
adityaviren <adityaviren@gmail.com>
2020-09-24 15:54:01 +0530

    UC6 deployed in main

--- src/snakeAndLadder/SnakeAndLadder.java
+++ src/snakeAndLadder/SnakeAndLadder.java
@@ -4,19 +4,21 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int position = 0, temp_change_position, temp_position;
+		int position = 0, temp_change_position, temp_position,no_of_dice_rolls=0;
 		while (position != 100) {
+			no_of_dice_rolls++;
 			temp_position = position;
 			temp_change_position = diceRoll() * positionRoll();
 			temp_position += temp_change_position;
 			if (temp_position <= 0) {
 				position = 0;
-			} else if (temp_position >= 100) {
+			} else if (temp_position > 100) {
 			} else {
 				position = temp_position;
 			}
+			System.out.println(position);
 		}
-		System.out.println("You Won");
+		System.out.println("You Won in "+no_of_dice_rolls+" dice rolls.");
 	}
 
 	private static int diceRoll() {
