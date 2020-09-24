1312348075c6d2b53a318132aaa201d8897e99f9
adityaviren <adityaviren@gmail.com>
2020-09-24 14:36:45 +0530

    Initial commit

--- src/snakeAndLadder/SnakeAndLadder.java
+++ src/snakeAndLadder/SnakeAndLadder.java
@@ -1,10 +1,12 @@
 package snakeAndLadder;
-
+import java.util.Scanner;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		int position=0;
+		
 	}
 
 }
