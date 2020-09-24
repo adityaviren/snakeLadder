bf08f38c83011ac4979dcc4ffd518b817a053021
adityaviren <adityaviren@gmail.com>
2020-09-24 15:45:13 +0530

    UC5 deployed in main with condition position!=100

--- src/snakeAndLadder/SnakeAndLadder.java
+++ src/snakeAndLadder/SnakeAndLadder.java
@@ -4,51 +4,59 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int position=0,temp_change_position;
-		while(position <= 100) {
-			temp_change_position=diceRoll()*positionRoll();
-			position+=temp_change_position;
-			if(position<=0)
-				position=0;
+		int position = 0, temp_change_position, temp_position;
+		while (position != 100) {
+			temp_position = position;
+			temp_change_position = diceRoll() * positionRoll();
+			temp_position += temp_change_position;
+			if (temp_position <= 0) {
+				position = 0;
+			} else if (temp_position >= 100) {
+			} else {
+				position = temp_position;
+			}
 		}
+		System.out.println("You Won");
 	}
+
 	private static int diceRoll() {
-		int random_generator = (int) ((Math.random()*60)%6);
+		int random_generator = (int) ((Math.random() * 60) % 6);
 		int dice_roll;
 		switch (random_generator) {
 		case 1:
-			dice_roll=1;
+			dice_roll = 1;
 			break;
 		case 2:
-			dice_roll=2;
+			dice_roll = 2;
 			break;
 		case 3:
-			dice_roll=3;
+			dice_roll = 3;
 			break;
 		case 4:
-			dice_roll=4;
+			dice_roll = 4;
 			break;
 		case 5:
-			dice_roll=5;
+			dice_roll = 5;
 			break;
-		default :
-			dice_roll=6;
+		default:
+			dice_roll = 6;
 			break;
 		}
 		return dice_roll;
 	}
+
 	private static int positionRoll() {
-		int pos_generator = (int) ((Math.random()*30)%3);
+		int pos_generator = (int) ((Math.random() * 30) % 3);
 		int pos_roll;
 		switch (pos_generator) {
 		case 1:
-			pos_roll=1;
+			pos_roll = 1;
 			break;
 		case 2:
-			pos_roll=-1;
+			pos_roll = -1;
 			break;
-		default :
-			pos_roll=0;
+		default:
+			pos_roll = 0;
 			break;
 		}
 		return pos_roll;
