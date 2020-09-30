261917623e24786370a3b2a0a79776093d0d2f52
unknown <adityaviren@gmail.com>
2020-09-30 09:12:35 +0530

    added position

--- src/snakeAndLadder/SnakeAndLadder.java
+++ src/snakeAndLadder/SnakeAndLadder.java
@@ -38,35 +38,6 @@ public class SnakeAndLadder {
 		System.out.println(message+" won the game in "+(no_of_dice_rolls_1+no_of_dice_rolls_2)+" dice rolls");
 	}
 
-	private static int diceRoll() {
-		int random_generator = (int) ((Math.random() * 60) % 6);
-		int dice_roll;
-		switch (random_generator) {
-		case 1:
-			dice_roll = 1;
-			break;
-		case 2:
-			dice_roll = 2;
-			break;
-		case 3:
-			dice_roll = 3;
-			break;
-		case 4:
-			dice_roll = 4;
-			break;
-		case 5:
-			dice_roll = 5;
-			break;
-		case 0:
-			dice_roll = 6;
-			break;
-		default :
-			dice_roll=0;
-			break;
-		}
-		return dice_roll;
-	}
-
 	private static int positionRoll() {
 		int pos_generator = (int) ((Math.random() * 30) % 3);
 		int pos_roll;
@@ -83,5 +54,6 @@ public class SnakeAndLadder {
 		}
 		return pos_roll;
 	}
+	
 
 }
