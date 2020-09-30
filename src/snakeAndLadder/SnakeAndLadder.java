df04dabcf8f79f041d78a768fa95fdc2fc2ad266
unknown <adityaviren@gmail.com>
2020-09-30 10:25:19 +0530

    refactor no_of_rolls

--- src/snakeAndLadder/SnakeAndLadder.java
+++ src/snakeAndLadder/SnakeAndLadder.java
@@ -7,11 +7,10 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int position[] = new int[2];
-		int no_of_rolls[] = new int[2];
-		int temp_position,temp_change,dice_roll,position_roll,toggle=0; // 0 means player 1
+		int temp_position,temp_change,no_of_rolls=0,dice_roll,position_roll,toggle=0; // 0 means player 1
 		boolean toggle_player=false; // false means player 1
 		while (position[0] != 100 && position[1] !=100) {
-			no_of_rolls[toggle]++;
+			no_of_rolls++;
 			temp_position=position[toggle];
 			Random random = new Random();
 			dice_roll = (int) random.nextInt(6) + 1;
@@ -28,9 +27,9 @@ public class SnakeAndLadder {
 			toggle=(toggle_player)? 1 : 0;
 		}
 		if(position[0]==100)
-			System.out.println("Player 1 won the game in "+(no_of_rolls[0]+no_of_rolls[1]));
+			System.out.println("Player 1 won the game in "+no_of_rolls);
 		else
-			System.out.println("Player 2 won the game in "+(no_of_rolls[0]+no_of_rolls[1]));
+			System.out.println("Player 2 won the game in "+no_of_rolls);
 	}
 	
 }
