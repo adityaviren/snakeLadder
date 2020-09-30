4ee2abb3a0f2e0749fda9995aef8d3ceed7f6d4e
unknown <adityaviren@gmail.com>
2020-09-30 09:16:28 +0530

    added dice roll

--- src/snakeAndLadder/SnakeAndLadder.java
+++ src/snakeAndLadder/SnakeAndLadder.java
@@ -1,59 +1,14 @@
 package snakeAndLadder;
 
+import java.util.*;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int position_1 = 0, position_2 = 0, temp_change_position, temp_position, no_of_dice_rolls_1 = 0,no_of_dice_rolls_2=0;
-		boolean toggle_player = true; // true means player 1
-		while (position_1 != 100 && position_2 != 100) {
-			if (toggle_player) {
-				no_of_dice_rolls_1++;
-				temp_position = position_1;
-				temp_change_position = diceRoll() * positionRoll();
-				temp_position += temp_change_position;
-				if (temp_position <= 0)
-					position_1 = 0;
-				else if (temp_position > 100) {
-				} else
-					position_1 = temp_position;
-				toggle_player = (temp_change_position >0) ? true : false ;
-				System.out.println("Position of player 1 is "+position_1);
-			}
-			else {
-				no_of_dice_rolls_2++;
-				temp_position = position_2;
-				temp_change_position = diceRoll() * positionRoll();
-				temp_position += temp_change_position;
-				if (temp_position <= 0)
-					position_2 = 0;
-				else if (temp_position > 100) {
-				} else
-					position_2 = temp_position;
-				toggle_player = (temp_change_position >0) ? false : true ;
-				System.out.println("Position of player 2 is "+position_2);
-			}
-		}
-		String message = (position_1==100) ? "Player 1 " : "Player 2 ";
-		System.out.println(message+" won the game in "+(no_of_dice_rolls_1+no_of_dice_rolls_2)+" dice rolls");
+		int position = 0;
+		System.out.println("position is "+position);
+		Random random=new Random();
+		int dice_roll= (int) random.nextInt(6) +1;
+		System.out.println("Dice shows "+dice_roll);
 	}
-
-	private static int positionRoll() {
-		int pos_generator = (int) ((Math.random() * 30) % 3);
-		int pos_roll;
-		switch (pos_generator) {
-		case 1:
-			pos_roll = 1;
-			break;
-		case 2:
-			pos_roll = -1;
-			break;
-		default:
-			pos_roll = 0;
-			break;
-		}
-		return pos_roll;
-	}
-	
-
 }
